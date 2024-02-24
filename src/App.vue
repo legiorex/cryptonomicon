1d2eb4db5ff01c9410efe0c8aadc6dd385cd68b1
legiorex <batyushev@gmail.com>
2024-02-24 21:00:06 +0300

    lesson2

--- src/App.vue
+++ src/App.vue
@@ -14,7 +14,7 @@
             <div class="mt-1 relative rounded-md shadow-md">
               <input
                 v-model="tickerName"
-                @keydown.enter="addTicker"
+                @keydown.enter="addTicker(tickerName)"
                 type="text"
                 name="wallet"
                 id="wallet"
@@ -48,7 +48,7 @@
           </div>
         </div>
         <button
-          @click="addTicker"
+          @click="addTicker(tickerName)"
           type="button"
           class="my-4 inline-flex items-center py-2 px-4 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-full text-white bg-gray-600 hover:bg-gray-700 transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
         >
@@ -68,12 +68,14 @@
           Добавить
         </button>
       </section>
-      <template v-if="!!tickers.length">
+      <template v-if="!!Object.values(tickers).length">
         <hr class="w-full border-t border-gray-600 my-4" />
         <dl class="mt-5 grid grid-cols-1 gap-5 sm:grid-cols-3">
           <div
             v-for="item in tickers"
             :key="item.id"
+            @click="selectTicker(item)"
+            :class="{ 'border-4': sell && sell.id === item.id }"
             class="bg-white overflow-hidden shadow rounded-lg border-purple-800 border-solid cursor-pointer"
           >
             <div class="px-4 py-5 sm:p-6 text-center">
@@ -82,7 +84,7 @@
             </div>
             <div class="w-full border-t border-gray-200"></div>
             <button
-              @click="removeTicker(item.id)"
+              @click.stop.prevent="removeTicker(item.id)"
               class="flex items-center justify-center font-medium w-full bg-gray-100 px-4 py-4 sm:px-6 text-md text-gray-500 hover:text-gray-600 hover:bg-gray-200 hover:opacity-20 transition-all focus:outline-none"
             >
               <svg
@@ -104,6 +106,38 @@
         </dl>
         <hr class="w-full border-t border-gray-600 my-4" />
       </template>
+      <section v-if="!!sell" class="relative">
+        <h3 class="text-lg leading-6 font-medium text-gray-900 my-8">{{ sell.name }} - USD</h3>
+        <div class="flex items-end border-gray-600 border-b border-l h-64">
+          <div class="bg-purple-800 border w-10 h-24"></div>
+          <div class="bg-purple-800 border w-10 h-32"></div>
+          <div class="bg-purple-800 border w-10 h-48"></div>
+          <div class="bg-purple-800 border w-10 h-16"></div>
+        </div>
+        <button @click="unSelectTicker" type="button" class="absolute top-0 right-0">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            xmlns:xlink="http://www.w3.org/1999/xlink"
+            xmlns:svgjs="http://svgjs.com/svgjs"
+            version="1.1"
+            width="30"
+            height="30"
+            x="0"
+            y="0"
+            viewBox="0 0 511.76 511.76"
+            style="enable-background: new 0 0 512 512"
+            xml:space="preserve"
+          >
+            <g>
+              <path
+                d="M436.896,74.869c-99.84-99.819-262.208-99.819-362.048,0c-99.797,99.819-99.797,262.229,0,362.048    c49.92,49.899,115.477,74.837,181.035,74.837s131.093-24.939,181.013-74.837C536.715,337.099,536.715,174.688,436.896,74.869z     M361.461,331.317c8.341,8.341,8.341,21.824,0,30.165c-4.16,4.16-9.621,6.251-15.083,6.251c-5.461,0-10.923-2.091-15.083-6.251    l-75.413-75.435l-75.392,75.413c-4.181,4.16-9.643,6.251-15.083,6.251c-5.461,0-10.923-2.091-15.083-6.251    c-8.341-8.341-8.341-21.845,0-30.165l75.392-75.413l-75.413-75.413c-8.341-8.341-8.341-21.845,0-30.165    c8.32-8.341,21.824-8.341,30.165,0l75.413,75.413l75.413-75.413c8.341-8.341,21.824-8.341,30.165,0    c8.341,8.32,8.341,21.824,0,30.165l-75.413,75.413L361.461,331.317z"
+                fill="#718096"
+                data-original="#000000"
+              ></path>
+            </g>
+          </svg>
+        </button>
+      </section>
     </div>
   </div>
 </template>
@@ -111,9 +145,9 @@
 <style src="./app.css" scoped></style>
 
 <script setup lang="ts">
-import { computed, reactive, ref, type Ref } from "vue"
+import { reactive, ref, type Ref } from "vue"
 
-type AddTicker = () => void
+type AddTicker = (newTicker: string) => void
 type RemoveTicker = (id: number) => void
 type Ticker = {
   name: string
@@ -121,15 +155,42 @@ type Ticker = {
   id: number
 }
 
-const tickers: Ref<Ticker[]> = ref([])
+const tickers: Record<number, Ticker> = reactive({})
 const tickerName: Ref<string> = ref("")
+const sell: Ref<null | Ticker> = ref(null)
+// let interval: number
 
-const addTicker: AddTicker = () => {
-  tickers.value.push({ name: tickerName.value, price: 0, id: new Date().getTime() })
+const addTicker: AddTicker = async (newTicker: string) => {
+  const id = new Date().getTime()
+  setInterval(async () => {
+    await fetch(`https://min-api.cryptocompare.com/data/price?fsym=${newTicker}&tsyms=USD`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        authorization: "8eeac640f75e175be422bd2010f05530ebc854f8a3d5eed849ace17b045c4f72"
+      }
+    }).then(async (res) => {
+      const data: { USD: number } = await res.json()
+
+      tickers[id] = { ...tickers[id], price: data.USD }
+    })
+  }, 3000)
+
+  tickers[id] = { name: newTicker, price: 0, id }
   tickerName.value = ""
 }
 
+const selectTicker = (ticker: Ticker) => {
+  sell.value = ticker
+}
+
+const unSelectTicker = () => {
+  sell.value = null
+}
+
 const removeTicker: RemoveTicker = (id: number) => {
-  tickers.value = tickers.value.filter((item) => item.id !== id)
+  delete tickers[id]
+  sell.value = null
+  // clearInterval(interval)
 }
 </script>
